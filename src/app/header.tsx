9a9b06a5d618a692a54761e9d436840587bcf3d4
Aidan Timson <contact@timmo.xyz>
2024-07-20 18:02:49 +0100

    Add edit button link to header

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -4,6 +4,8 @@ import { usePathname } from "next/navigation";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 import { ThemePicker } from "~/components/theme-picker";
+import { Button } from "~/components/ui/button";
+import Link from "next/link";
 
 export function Header() {
   const pathname = usePathname();
@@ -13,12 +15,30 @@ export function Header() {
     [pathname],
   );
 
+  const isOnDashboard = useMemo<boolean>(
+    () => (pathname.startsWith("/dashboard/") ? true : false),
+    [pathname],
+  );
+
+  console.log({
+    pathname,
+    isOnMainLayout,
+    isOnDashboard,
+  });
+
   return (
     <header
       className={`flex flex-row justify-between gap-2 p-2 ${isOnMainLayout ? "bg-slate-900" : "bg-gradient-to-b from-slate-900"}`}
     >
       <div className="flex items-center gap-2"></div>
       <div className="flex flex-row items-center gap-2">
+        {isOnDashboard && (
+          <Link href={`${pathname}/edit`}>
+            <Button variant="secondary" size="sm">
+              Edit
+            </Button>
+          </Link>
+        )}
         <ThemePicker />
         <SignedOut>
           <div className="transform rounded-md bg-violet-900 bg-opacity-60 px-4 py-2 text-white shadow-lg drop-shadow-2xl transition duration-300 hover:scale-105">
