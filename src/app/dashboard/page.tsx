e62e3e4e4a8d36c3d8aad893a44a9d6ab0d48677
Aidan Timson <contact@timmo.xyz>
2024-07-20 17:28:29 +0100

    Cleanup

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,9 +6,7 @@ import Link from "next/link";
 import { Button } from "~/components/ui/button";
 import {
   Card,
-  CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "~/components/ui/card";
