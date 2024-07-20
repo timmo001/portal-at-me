733b469266f909c1ac8af1532d2f58f0801c87a5
Aidan Timson <contact@timmo.xyz>
2024-07-20 17:28:40 +0100

    Dashboard view

--- src/app/dashboard/[id]/page.tsx
+++ src/app/dashboard/[id]/page.tsx
@@ -1,10 +1,46 @@
-export default async function Dashboard() {
+import { auth } from "@clerk/nextjs/server";
+import { notFound } from "next/navigation";
+
+import { api } from "~/trpc/server";
+
+export default async function Dashboard({
+  params,
+}: {
+  params: { id: number };
+}) {
+  const { userId } = auth();
+  if (!userId) notFound();
+
+  console.log({ params });
+  const dashboard = await api.dashboard.getDashboard(Number(params.id));
+
+  void api.dashboard.getDashboard.prefetch(params.id);
+
+  // Ensure the dashboard exists and belongs to the user
+  if (!dashboard || dashboard.userId !== userId) notFound();
+
   return (
     <>
       <section className="w-lg container flex min-h-64 flex-col items-center justify-center">
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Portal<span className="text-indigo-600">@</span>Me
+          {dashboard.name}
         </h1>
+        <h2 className="text-xl font-semibold text-gray-500">
+          {dashboard.description}
+        </h2>
+      </section>
+      <section className="w-lg container flex min-h-64 flex-col items-center justify-center">
+        {dashboard.dashboardLinks.map((link) => (
+          <a
+            key={link.id}
+            href={link.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 hover:underline"
+          >
+            {link.title}
+          </a>
+        ))}
       </section>
     </>
   );
